10c773bb0858795226045c18d9039cdc609315db
Ivan <sidorovich.vanya969@gmail.com>
2021-01-20 17:03:50 +0300

    Home Work6

--- Sidorovich Ivan HW6/main.js
+++ Sidorovich Ivan HW6/main.js
@@ -10,37 +10,39 @@ Vue.component('search', {
             searchLine: ''
         }
     },
-    props: ['goods'],
     methods: {
         filteredGoods() {
-            const regexp = new RegExp(this.searchLine, 'i');
-            this.filteredGoodsARR = this.goods.filter(res => regexp.test(res.product_name))
-            console.log(this.filteredGoodsARR.length);
+            this.$emit('search', this.searchLine);
         }
     }
-
 });
 
 Vue.component('catalog', {
     template: ` <div class="goods" >
-    <p v-if="filteredGoodsARR==0">Товар не найден</p>
-    <catalog-item v-bind:cart="cart" v-bind:filteredGoodsARR="filteredGoodsARR" v-for="(item, id) in filteredGoodsARR" :name="item.product_name" :id="id" :price="item.price"></catalog-item>
+    <p v-if="filteredGoods==0">Товар не найден</p>
+    <catalog-item v-for="(item, id) in filteredGoods" :name="item.product_name" :id="id" :price="item.price" :goodsItem="goods" :filteredGoodsARRItem="filteredGoodsARR" :cartItem="cart"></catalog-item>
     </div>`,
+    props: ['filter', 'cart'],
     data() {
         return {
             goods: [],
             filteredGoodsARR: []
-            
         }
     },
-    props: ['cart'],
-
+    computed: {
+        filteredGoods() {
+            const regexp = new RegExp(this.filter, 'i');
+            console.log('приняли регексп');
+            return this.filteredGoodsARR = this.goods.filter(res => regexp.test(res.product_name))
+        }
+    },
     mounted() {
         fetch(
             'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json'
         ).then(res => res.json()).then(res => {
             this.goods = [...res, ...res];
-            this.filteredGoodsARR = [...res, ...res]
+            this.filteredGoodsARR = [...res, ...res];
+            console.log(this.filteredGoodsARR);
         });
     }
 });
@@ -51,10 +53,11 @@ Vue.component('catalog-item', {
     <p>{{price}}</p>
     <button   @click="() => addCart(id)">Добавить</button>
     </div>`,
-    props: ['name', 'cart', 'price', 'id', 'filteredGoodsARR'],
+    props: ['name', 'price', 'id', 'filteredGoodsARRItem', 'goodsItem', 'cartItem'],
     methods: {
         addCart(id) {
-            this.cart.push(this.filteredGoodsARR[id]);
+            this.cartItem.push(this.filteredGoodsARRItem[id]);
+            console.log(this.cartItem);
         }
     },
 })
@@ -63,38 +66,47 @@ Vue.component('cart', {
     template: ` <div class="cart"  >
     <h2>Корзина</h2>
     <p v-if="cart.length ==0">Корзина пуста</p>
-    <div v-for="({product_name, price}, id) in cart">
-        <hr>
-        <h3>{{product_name}}</h3>
-        <p>{{price}}</p>
-        <button type="button" @click="() => deleteCart(id)">Убрать из корзины</button>
-        <hr>
-    </div>
+    <cartDiv v-for="(item, id) in cartArr" :cart="cart" :name="item.product_name" :price="item.price" :id="id"></cartDiv>
     <p v-if="cart.length !=0">В корзине {{cartGoods}} товаров на сумму {{sumCart}} </p>
 </div>`,
-    data(){
-        return {
-            cart: []
+    props: ['cart'],
+    computed: {
+        sumCart() {
+            return this.cart.reduce((total, item) => total + item.price, 0)
+        },
+        cartGoods() {
+            return this.cart.length
+        },
+        cartArr() {
+            return this.cart
         }
     }
+})
 
+Vue.component('cartDiv', {
+    template: `<div>
+    <h3>{{name}}</h3>
+    <p>{{price}}</p>
+    <button type="button" @click="() => deleteCart(id)">Убрать из корзины</button>
+    <hr>
+</div>`,
+    props: ['cart', 'name', 'price', 'id'],
+    methods: {
+        deleteCart(id) {
+            this.cart.splice(id, 1)
+        }
+    }
 })
 
 const app = new Vue({
     el: '#app',
     data: {
         cart: [],
+        searchLine: ''
     },
-    /* computed: {
-         sumCart() {
-             return this.cart.reduce((total, item) => total + item.price, 0)
-         },
-         cartGoods() {
-             return this.cart.length
-         },
-         deleteCart(id) {
-             this.cart.splice(id, 1);
-         }
-     },*/
-
+    methods: {
+        filteredGoodsMain(searchLine) {
+            this.searchLine = searchLine;
+        }
+    }
 });
